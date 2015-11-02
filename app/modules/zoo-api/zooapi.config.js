85021fe683c7b75758ba999356abb1926d70a59a
Simone Duca <Simo@simones-mbp.nat.physics.ox.ac.uk>
2015-11-02 15:42:14 +0000

    Update appid to match oauth. Add styling for login

--- app/modules/zoo-api/zooapi.config.js
+++ app/modules/zoo-api/zooapi.config.js
@@ -5,5 +5,5 @@ require('./zooapi.module.js')
         project_id: '376',
         workflow_id: '205',
         // https://github.com/zooniverse/Panoptes-Front-End/blob/master/app/api/config.coffee#L9
-        app_id: 'f79cf5ea821bb161d8cbb52d061ab9a2321d7cb169007003af66b43f7b79ce2a'
+        app_id: '2abed6ac014d4a8416a1f578de922600451a10c759dfeab6c9f0edbeb91c8f88' //'f79cf5ea821bb161d8cbb52d061ab9a2321d7cb169007003af66b43f7b79ce2a'
     });
