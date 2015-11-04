fe40d0673a9347b7701b66fc9b72493538a92fd6
Simone Duca <Simo@simones-mbp.nat.physics.ox.ac.uk>
2015-11-04 14:25:04 +0000

    Save project object in memory.

--- app/modules/zoo-api/zooapi-project.factory.js
+++ app/modules/zoo-api/zooapi-project.factory.js
@@ -7,6 +7,7 @@ require('./zooapi.module.js')
 function zooAPIProject($q, localStorageService, zooAPIConfig, zooAPI) {
 
     var factory;
+    var _data;
 
     factory = {
         get: get
@@ -14,10 +15,16 @@ function zooAPIProject($q, localStorageService, zooAPIConfig, zooAPI) {
 
     return factory;
 
-    function get() {
+    function _getFromPanoptes() {
+        console.log('getting from panoptes')
         return zooAPI.type('projects').get(zooAPIConfig.project_id)
             .then(function (response) {
+                _data = response;
                 return response;
             });
+    }
+
+    function get() {
+        return (_data) ? $q.when(_data) : _getFromPanoptes();
     }
 }
