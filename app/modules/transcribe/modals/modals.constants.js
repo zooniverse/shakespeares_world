86d2ac4f8a387b9f65220a0d609b8c06eb62b08f
Simone Duca <Simo@simones-mbp.nat.physics.ox.ac.uk>
2015-11-05 10:07:27 +0000

    Add tutorials.

--- app/modules/transcribe/modals/modals.constants.js
+++ app/modules/transcribe/modals/modals.constants.js
@@ -7,6 +7,36 @@ require('./modals.module.js')
             controller: 'TranscribeNextController',
             controllerAs: 'vm',
             size: 'md',
-            backdrop: 'static'
+            backdrop: 'static',
+            windowClass: 'next-modal'
+        },
+        signIn: {
+            templateUrl: 'modals/sign-in.html',
+            controller: 'SignInController',
+            controllerAs: 'vm',
+            size: 'md',
+            backdrop: 'static',
+            windowClass: 'signin-modal'
+        },
+        talk: {
+            templateUrl: 'modals/talk.html',
+            controller: 'TalkController',
+            controllerAs: 'vm',
+            size: 'md',
+            windowClass: 'talk-modal'
+        },
+        tutorial: {
+            templateUrl: 'modals/tutorial.html',
+            controller: 'TutorialController',
+            controllerAs: 'vm',
+            size: 'md',
+            windowClass: 'tutorial-modal'
+        },
+        expired: {
+            templateUrl: 'modals/expired.html',
+            controller: 'ExpiredController',
+            controllerAs: 'vm',
+            size: 'md',
+            windowClass: 'expired-modal'
         }
     });
