064c713ecd50b148240c49671ad7e5a26a78aa78
Simone Duca <Simo@simones-mbp.nat.physics.ox.ac.uk>
2015-11-09 12:13:00 +0000

    Remove empty function.

--- app/modules/transcribe/modals/variants.controller.js
+++ app/modules/transcribe/modals/variants.controller.js
@@ -4,19 +4,11 @@ require('./modals.module.js')
     .controller('VariantsController', VariantsController);
 
 // @ngInject
-function VariantsController($modalInstance, AnnotationsFactory) {
+function VariantsController($modalInstance) {
     var vm = this;
     vm.close = close;
-    vm.data = AnnotationsFactory.list();
-    vm.variants = getVariants;
 
     function close(result) {
         $modalInstance.close(result);
     }
-
-    function getVariants() {
-
-    }
-
-
 }
