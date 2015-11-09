716730238971b26a8cf24f34936b175fc0fdea03
Simone Duca <Simo@simones-mbp.nat.physics.ox.ac.uk>
2015-11-09 16:01:47 +0000

    Close #133

--- app/modules/home/genre.constants.js
+++ app/modules/home/genre.constants.js
@@ -4,53 +4,7 @@ require('./home.module.js')
     .constant('GenreListConstants', [
         {
             active: true,
-            genreId: "1",
-            name: "Family Papers"
-        },
-        {
-            active: true,
             genreId: "2",
-            name: "Literary Works"
-        },
-        {
-            active: true,
-            genreId: "3",
-            name: "Letters"
-        },
-        {
-            active: true,
-            genreId: "4",
-            name: "Newsletters"
-        },
-        {
-            active: true,
-            genreId: "5",
-            name: "Miscellanies"
-        },
-        {
-            active: true,
-            genreId: "6",
-            name: "Recipe Books"
-        },
-        {
-            active: true,
-            genreId: "7",
-            name: "Sermons"
-        },
-        {
-            active: true,
-            genreId: "9",
-            name: "Diaries"
-        },
-        {
-            active: true,
-            genreId: "10",
-            name: "Diplomatic Papers"
-        },
-        {
-            active: true,
-            genreId: "11",
-            name: "Coats of Arms"
+            name: "Beta Test"
         }
-
     ]);
