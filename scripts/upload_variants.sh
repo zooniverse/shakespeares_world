a82b4e8d3fd684600d640d538491b7a274bddc91
Simone Duca <simone@zooniverse.org>
2016-08-30 12:36:38 +0100

    Fix #306

--- scripts/upload_variants.sh
+++ scripts/upload_variants.sh
@@ -7,7 +7,7 @@ then
 fi
 
 VARIANTS_PATH="$1"
-DEPLOY_PATH="s3://zooniverse-static/www.shakespearesworld.org/variants/"
+DEPLOY_PATH="s3://zooniverse-static/variants.shakespearesworld.org/"
 SYNC_CMD="aws s3 sync ${VARIANTS_PATH} ${DEPLOY_PATH}"
 echo "running sync cmd: ${SYNC_CMD}"
 ${SYNC_CMD}
