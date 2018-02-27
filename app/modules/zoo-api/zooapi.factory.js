2caa3874c9c5785501790903165f30f3786b8602
Simone Duca <simone@zooniverse.org>
2018-02-27 12:55:02 +0000

    Improve UX for token refresh failure

--- app/modules/zoo-api/zooapi.factory.js
+++ app/modules/zoo-api/zooapi.factory.js
@@ -4,20 +4,18 @@ require('./zooapi.module.js')
     .factory('zooAPI', zooAPI);
 
 var ApiClient = require('panoptes-client/lib/api-client');
-var OAuth = require('panoptes-client/lib/oauth')
+var oauth = require('panoptes-client/lib/oauth')
 // @ngInject
-function zooAPI(AnnotationsFactory, $location, $rootScope) {
+function zooAPI(AnnotationsFactory, $location) {
     ApiClient.beforeEveryRequest = function() {
-        return OAuth.checkBearerToken()
+        return oauth.checkBearerToken()
             .then(function (token) {
                 console.log('Token refreshed: ', token);
             })
             .catch(function (error) {
                 console.log('Failed to refresh token: ', error);
-                alert('Your session is about to expire. Press "OK" to save your work and get a new session.');
                 AnnotationsFactory.updateCache();
-                OAuth.signIn($location.absUrl());
-                $rootScope.$broadcast('auth:loginChange');
+                oauth.signIn($location.absUrl());
             })
     }
     // There's only a version of this project on production, so rather than
